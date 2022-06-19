81dd63e8efc83288fb0a55fd5ca09b75ad30de5b
ASriDatta <77008041+ASriDatta@users.noreply.github.com>
2022-06-19 12:40:50 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => { 
    e.waitUntil( 
-     caches.open('pwa tutorial').then((cache) => cache.addAll([
+     caches.open('pwa-tutorial').then((cache) => cache.addAll([
          '/index.html',
          '/tables.html',
          '/speak.html'
